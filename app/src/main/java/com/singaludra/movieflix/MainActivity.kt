4de81c19360b3689a55dac62deca2a67bbe141a7
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-02 23:28:23 +0700

    feat: setup nav graph

--- app/src/main/java/com/singaludra/movieflix/MainActivity.kt
+++ app/src/main/java/com/singaludra/movieflix/MainActivity.kt
@@ -2,10 +2,20 @@ package com.singaludra.movieflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.singaludra.movieflix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
     }
 }
