16f2c008837acfc2f704f1680564fc0965516dcd
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-02 23:53:59 +0700

    feat: initiate  and , attach them to ViewPagerAdapter

--- app/src/main/java/com/singaludra/movieflix/presentation/home/ViewPagerAdapter.kt
+++ app/src/main/java/com/singaludra/movieflix/presentation/home/ViewPagerAdapter.kt
@@ -4,6 +4,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.singaludra.movieflix.presentation.favorite.FavoriteFragment
+import com.singaludra.movieflix.presentation.movies.MovieFragment
 
 class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
@@ -15,9 +17,9 @@ class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     override fun createFragment(position: Int): Fragment {
         //TODO: add fragment once fragment is added
         return when (position) {
-            0 -> return Fragment()
-            1 -> return Fragment()
-            else -> Fragment()
+            0 -> return MovieFragment()
+            1 -> return FavoriteFragment()
+            else -> MovieFragment()
         }
     }
 
