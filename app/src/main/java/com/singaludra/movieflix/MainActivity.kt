b7720fcfddf88001ab36d978b5daf1298af88704
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-04 09:05:58 +0700

    feat: slicing favorite movie screen, create functionality to show movies

--- app/src/main/java/com/singaludra/movieflix/MainActivity.kt
+++ app/src/main/java/com/singaludra/movieflix/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.singaludra.movieflix.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
---
+++ app/src/main/java/com/singaludra/movieflix/feature/favorite/FavoriteViewModel.kt
@@ -0,0 +1,52 @@
+package com.singaludra.movieflix.feature.favorite
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.singaludra.domain.Resource
+import com.singaludra.domain.model.Movie
+import com.singaludra.domain.repository.IMovieRepository
+import com.singaludra.movieflix.feature.movies.MovieUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+sealed interface FavriteMovieUiState{
+    data class Success(val movie: List<Movie>): FavriteMovieUiState
+    data class Error(val exception: String): FavriteMovieUiState
+
+    object Loading: FavriteMovieUiState
+}
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    private val repository: IMovieRepository
+): ViewModel() {
+    private val _uiState = MutableStateFlow<FavriteMovieUiState>(FavriteMovieUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getMovies()
+    }
+    private fun getMovies() {
+        viewModelScope.launch {
+            repository.getFavoriteMovies().collect{ result ->
+                when(result) {
+                    is Resource.Success -> {
+                        if (result.data?.isNotEmpty() == true){
+                            _uiState.value = FavriteMovieUiState.Success(result.data!!)
+                        } else {
+                            _uiState.value = FavriteMovieUiState.Success(emptyList())
+                        }
+                    }
+                    is Resource.Error -> {
+                        _uiState.value = FavriteMovieUiState.Error(result.message ?: "undefined")
+                    }
+                    is Resource.Loading -> {
+                        _uiState.value = FavriteMovieUiState.Loading
+                    }
+                }
+            }
+        }
+    }
+}
