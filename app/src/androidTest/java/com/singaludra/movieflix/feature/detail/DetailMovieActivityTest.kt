d1716b22b39a9936f39986e7f26df01725b61551
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-10-05 08:57:11 +0700

    test: initiate instrumented test

---
+++ app/src/androidTest/java/com/singaludra/movieflix/feature/detail/DetailMovieActivityTest.kt
@@ -0,0 +1,20 @@
+package com.singaludra.movieflix.feature.detail
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class DetailMovieActivityTest{
+    @get:Rule
+    val activityRule = ActivityScenarioRule(DetailMovieActivity::class.java)
+
+   
+}
